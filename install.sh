9a4ee7eb1e9677cabff1c64bed197e2d684c300b
Gaurav Juvekar <gauravjuvekar@gmail.com>
2018-06-11 08:37:51 +0530

    Track ctags and ycm_extra_conf and install as symlinks

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git submodule update --init --recursive
 ln --symbolic "$SCRIPT_DIRECTORY/vimrc" "$HOME/.vimrc"
+ln --symbolic "$SCRIPT_DIRECTORY/ctags" "$HOME/.ctags"
+ln --symbolic "$SCRIPT_DIRECTORY/ycm_extra_conf.py" "$HOME/ycm_extra_conf.py"
