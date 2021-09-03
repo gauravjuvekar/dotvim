ae1f88babb8c2cd526b50c87c180e066c165384f
Gaurav Juvekar <gauravjuvekar@gmail.com>
2021-09-03 02:08:38 -0700

    nvim install

--- install.sh
+++ install.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+# set -eEuxo pipefail
 
-set -e
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SRC="$SCRIPT_DIRECTORY"
 
-git submodule update --init --recursive
-ln --symbolic "$SCRIPT_DIRECTORY/vimrc" "$HOME/.vimrc"
-ln --symbolic "$SCRIPT_DIRECTORY/ctags" "$HOME/.ctags"
-ln --symbolic "$SCRIPT_DIRECTORY/ycm_extra_conf.py" "$HOME/ycm_extra_conf.py"
+: ${XDG_CONFIG_HOME=$HOME/.config}
+
+function installf {
+    SRC="$1"
+    DST="$2"
+
+    mkdir -vp "$(dirname "$DST")"
+    ln -vfs "$SRC" "$DST"
+
+    # install -vDT "$SRC" "$DST"
+}
+
+
+installf "$SRC/nvimrc" "$XDG_CONFIG_HOME/nvim/init.vim"
+
+# git submodule update --init --recursive
+
+installf "$SRC/vimrc" "$HOME/.vimrc"
+installf "$SRC/ctags" "$HOME/.ctags"
+installf "$SRC/ycm_extra_conf.py" "$HOME/ycm_extra_conf.py"
