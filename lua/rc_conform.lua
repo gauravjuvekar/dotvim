a4d6aaf00dbc64de93a2864ee05a11dfd6beb891
Gaurav Juvekar <gauravjuvekar@gmail.com>
2024-01-17 10:50:46 -0800

    conform: use clang-format for c/cpp

--- lua/rc_conform.lua
+++ lua/rc_conform.lua
@@ -2,6 +2,8 @@ local conform = require("conform")
 
 conform.setup({
     formatters_by_ft = {
+        c = { "clang_format" },
+        cpp = { "clang_format" },
         lua = { "stylua" },
         python = { "isort", "yapf" },
         rust = { "rustfmt" },
