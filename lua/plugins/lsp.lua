bde7b31fbda4de7440857512dbce15fa1449825f
Gaurav Juvekar <gauravjuvekar@gmail.com>
2024-11-10 12:13:25 -0800

    coq: migrate config to lazy

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,9 +19,39 @@ return {
   },
   init = function()
     vim.g.coq_settings = {
-      auto_start = false, -- if you want to start COQ at startup
-      -- Your COQ settings here
+      auto_start = "shut-up",
+      keymap = {
+        recommended = false,
+        jump_to_mark = "<c-l>",
+      },
     }
+
+    -- keybinds unrelated to nvim-autopairs. CR and BS have special handling in
+    -- autopairs.lua to avoid them stepping on each other
+    vim.api.nvim_set_keymap(
+      "i",
+      "<esc>",
+      [[pumvisible() ? "<c-e><esc>" : "<esc>"]],
+      { expr = true, noremap = true, silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      "i",
+      "<c-c>",
+      [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]],
+      { expr = true, noremap = true, silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      "i",
+      "<tab>",
+      [[pumvisible() ? "<c-n>" : "<tab>"]],
+      { expr = true, noremap = true, silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      "i",
+      "<s-tab>",
+      [[pumvisible() ? "<c-p>" : "<bs>"]],
+      { expr = true, noremap = true, silent = true }
+    )
   end,
   config = function()
     local lspconfig = require("lspconfig")
