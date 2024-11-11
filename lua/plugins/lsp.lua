2d640113948818b4fb335a8d078bd8e82fe98a47
Gaurav Juvekar <gauravjuvekar@gmail.com>
2024-11-11 00:50:08 -0800

    lsp: move config to lazy

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -52,6 +52,22 @@ return {
       [[pumvisible() ? "<c-p>" : "<bs>"]],
       { expr = true, noremap = true, silent = true }
     )
+
+    vim.keymap.set("n", "[e", vim.diagnostic.goto_prev)
+    vim.keymap.set("n", "]e", vim.diagnostic.goto_next)
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts)
+      end,
+    })
   end,
   config = function()
     local lspconfig = require("lspconfig")
