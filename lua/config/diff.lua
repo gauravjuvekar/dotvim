c2288f387ea312f9bf74286edde14393dea53e54
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-03-26 00:10:18 -0700

    diff: fix shortcut to fetch REMOTE hunk

--- lua/config/diff.lua
+++ lua/config/diff.lua
@@ -1,7 +1,7 @@
 -- 3 way merge commands
 vim.api.nvim_set_keymap("n", "ml", ":diffget LOCAL<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "mb", ":diffget BASE<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "mb", ":diffget REMOTE<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "mr", ":diffget REMOTE<cr>", { noremap = true })
 
 local group = vim.api.nvim_create_augroup("diffMode", { clear = true })
 vim.api.nvim_create_autocmd("VimEnter", {
