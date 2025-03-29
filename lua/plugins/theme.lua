0fc2f6757619c502b5e2b446b48ac30fdbd21db5
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-03-28 17:26:40 -0700

    theme: make search matches more visible

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,6 +4,20 @@ return {
   config = function()
     require("nightfox").setup({
       options = {},
+      palettes = {
+        carbonfox = {
+          -- set yellow/orange to actually be yellow/orange
+          yellow = require("nightfox.palette").load("duskfox").yellow,
+          orange = require("nightfox.palette").load("nightfox").orange,
+        },
+      },
+      groups = {
+        all = {
+          -- better highlight for search results
+          Search = { link = "IncSearch" },
+          CurSearch = { fg = "palette.bg1", bg = "palette.cyan" },
+        },
+      },
     })
     vim.cmd("colorscheme carbonfox")
   end,
