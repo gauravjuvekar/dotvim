e7c8fa54790dac313202d2884cbe954af56d0013
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-05-10 10:01:06 -0700

    codeium: only on nvidia.com domain

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -1,11 +1,17 @@
-return {
-  "Exafunction/windsurf.vim",
-  config = function ()
-    -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set('i', '<C-a>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-    vim.keymap.set('i', '<C-s>', function () return vim.fn['codeium#AcceptNextLine']() end, { expr = true, silent = true })
-    vim.keymap.set('i', '<c-j>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-    vim.keymap.set('i', '<c-l>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-    vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-  end
-}
+local fqdn = string.gsub(vim.fn.system('hostname -f'), "%s+", "")
+if string.match(fqdn, [[%.nvidia%.com$]])
+then
+  return {
+    "Exafunction/windsurf.vim",
+    config = function ()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-a>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-s>', function () return vim.fn['codeium#AcceptNextLine']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-j>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-l>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
+  }
+else
+  return {}
+end
